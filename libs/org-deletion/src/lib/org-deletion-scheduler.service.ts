e58d47418c17d89931d531889730daa5e444f8c6
Piercarlo Serena <piercarlo.serena@gmail.com>
2026-03-18 09:38:01 +0100

    fix(org-deletion): add undefined parameter to requestDeletion for better handling

--- libs/org-deletion/src/lib/org-deletion-scheduler.service.ts
+++ libs/org-deletion/src/lib/org-deletion-scheduler.service.ts
@@ -65,6 +65,7 @@ export class OrgDeletionSchedulerService {
           await this.deletionService.requestDeletion(
             org.id,
             DeletionTrigger.SUBSCRIPTION_EXPIRY,
+            undefined,
           );
 
           this.logger.log(
